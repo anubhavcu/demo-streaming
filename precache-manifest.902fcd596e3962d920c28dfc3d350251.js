31798374765d0b5f4239fea29bfc673bb5bbb98b
anubhavcu <anubhav342y@gmail.com>
2020-06-09 16:55:35 +0530

    Updates

---
+++ precache-manifest.902fcd596e3962d920c28dfc3d350251.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472373f17e4d4a9f5ebf7b2db039162",
+    "url": "/demo-streaming/index.html"
+  },
+  {
+    "revision": "f06a9f5c9e603c69d3ca",
+    "url": "/demo-streaming/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56316a115faa25fce301",
+    "url": "/demo-streaming/static/css/main.1620a37f.chunk.css"
+  },
+  {
+    "revision": "f06a9f5c9e603c69d3ca",
+    "url": "/demo-streaming/static/js/2.d51de08f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-streaming/static/js/2.d51de08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56316a115faa25fce301",
+    "url": "/demo-streaming/static/js/main.bcd892b8.chunk.js"
+  },
+  {
+    "revision": "d824b39fc48175bfbb4d",
+    "url": "/demo-streaming/static/js/runtime-main.6061cdf6.js"
+  },
+  {
+    "revision": "f4824b73495270efeb1a5438f972e1a5",
+    "url": "/demo-streaming/static/media/apple-app-store.f4824b73.svg"
+  },
+  {
+    "revision": "5e04d7fe979306b4f28dc6ceaf9bae1d",
+    "url": "/demo-streaming/static/media/movie.5e04d7fe.jpg"
+  },
+  {
+    "revision": "4bbd33fe50032ad67a47d969a93da6cc",
+    "url": "/demo-streaming/static/media/play-store-logo.4bbd33fe.svg"
+  },
+  {
+    "revision": "fc0e2a5b18d4a662717f3cbb1827871d",
+    "url": "/demo-streaming/static/media/series.fc0e2a5b.jpg"
+  },
+  {
+    "revision": "ada84d8a1bc3d9cf495ad1db4068b3d1",
+    "url": "/demo-streaming/static/media/windows-store.ada84d8a.svg"
+  }
+]);
